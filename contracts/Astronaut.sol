61f16405d754d6a4a5de4072ff26269999f849b0
Paul Fletes <paul.fletes@gmail.com>
2024-12-15 17:09:45 -0600

    Init project w/ contract and assets

---
+++ contracts/Astronaut.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract AstronautNFT is ERC721URIStorage, Ownable {
+    constructor() ERC721("AstronautNFT", "ASTRO") Ownable(msg.sender) {}
+
+    function mint(
+        address _to,
+        uint256 _tokenId,
+        string calldata _uri
+    ) external onlyOwner {
+        _mint(_to, _tokenId);
+        _setTokenURI(_tokenId, _uri);
+    }
+}
